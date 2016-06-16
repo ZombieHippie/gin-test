e702891b0c8d741a7b70ae35391dbc5937877429
Cole R Lawrence <colelawr@gmail.com>
2016-06-16 15:18:56 -0500

    Organize the postWebhook

--- src/app/app.go
+++ src/app/app.go
@@ -24,53 +24,7 @@ func Setup(db *gorm.DB) {
 	router := gin.Default()
 
 	// Create summary
-	router.POST("/webhook", func(c *gin.Context) {
-		var json summary.Summary
-
-		c.Bind(&json) // This will infer what binder to use depending on the content-type header.
-
-		hasRepoName := !shared.IsZero(json.Repository.Name)
-
-		if !hasRepoName {
-			c.JSON(http.StatusBadRequest, webhookResp{
-				Message: "Error: No Repository.Name specified."
-			})
-			return
-		}
-
-		json.Repository = repo.EnsureRepository(db, json.Repository)
-
-		hasCommit := !shared.IsZero(json.Commit)
-		hasCreated := !shared.IsZero(json.Created)
-		hasPullRequest := !shared.IsZero(json.PullRequestID)
-		hasArtifacts := len(json.Artifacts) > 0
-
-		var err error
-		if !hasArtifacts {
-			err = "No Artifacts present."
-		} else if !hasPullRequest {
-			err = "No PullRequestID specified."
-		} else if !hasCommit {
-			err = "No Commit SHA specified."
-		} else if !hasArtifacts {
-			err = "No Artifacts provided."
-		}
-
-		if err != nil {
-			c.JSON(http.StatusBadRequest, webhookResp{
-				Message: "Error: " + string(err)
-			})
-			return
-		}
-
-		created, result := summary.CreateSummary(json)
-		
-		status := created ? http.StatusCreated : http.StatusInternalServerError
-		c.JSON(status, webhookResp{
-			Message: "Successfully created summary.",
-			Summary: result,
-		})
-	})
+	router.POST("/webhook", postWebhook)
 
 	router.GET("/summary/list", func(c *gin.Context) {
 		sums := summary.GetAllSummaries(db)
