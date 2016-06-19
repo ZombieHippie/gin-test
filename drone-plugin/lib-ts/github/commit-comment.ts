94e1419bf613c928fa28cbb9379f59cb303b5ea3
Cole R Lawrence <colelawr@gmail.com>
2016-06-19 00:20:55 -0500

    Test comment success

---
+++ drone-plugin/lib-ts/github/commit-comment.ts
@@ -0,0 +1,73 @@
+/// <reference path="../../typings/index.d.ts" />
+/**
+ * From notes.
+
+## Creating a Commit Comment
+https://developer.github.com/v3/repos/comments/#create-a-commit-comment
+POST /repos/:owner/:repo/commits/:sha/comments
+### Input
+```
+Name      Type        Description
+body      string      Required. The contents of the comment.
+path      string      Relative path of the file to comment on.
+position  integer	    Line index in the diff to comment on.
+line      integer	    Deprecated. Use position parameter instead. Line number in the file to comment on.
+```
+### Example
+{
+  "body": "Great stuff",
+  "path": "file1.txt",
+  "position": 4,
+  "line": null
+}
+ 
+ */
+
+
+import request = require("request")
+import fs = require('fs')
+import { ENV } from '../../drone-parser'
+
+interface CommentPayload {
+  body:     string, // Required. The contents of the comment.
+  path:     string, // Relative path of the file to comment on.
+  position: number, // Line index in the diff to comment on.
+}
+
+const botkey = process.env.PLUGIN_BOT
+const sha = ENV.DRONE_COMMIT
+const repo = ENV.DRONE_REPO
+
+const host = `https://dryclean-bot:${botkey}@api.github.com`
+const endpoint = `/repos/${repo}/commits/${sha}/comments`
+
+const emoji = [
+  ":hocho:"
+]
+
+function PostComment(path: string, position: number, message: string, severity = 1, callback: (err, response) => any = null) {
+  console.log(endpoint)
+  console.log(botkey)
+  let options: request.OptionsWithUrl = {
+    url: host + endpoint,
+    headers: {
+      "User-Agent": "Drycleaner robot",
+    },
+    method: 'POST',
+    json: {
+      body: `${emoji[severity] || ":tshirt:"} ${message}`,
+      path: path,
+      position: position,
+    },
+  }
+
+  request(options, (error, httpresponse, body) => {
+    console.log(error, body)
+    try {
+      body = JSON.parse(body)
+    } catch (err) {} // guess it wasn't json...
+    callback && callback(error, body)
+  })
+}
+
+export { PostComment }
---
+++ drone-plugin/test-comment.ts
@@ -0,0 +1,5 @@
+
+import { PostComment } from './lib-ts/github/commit-comment'
+
+PostComment('config/protractor.conf.js', 11, ":cloud:", 0)
+PostComment('config/protractor.conf.js', 12, ":horse:", 1)
